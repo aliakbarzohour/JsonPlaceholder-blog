1d9badfc34faa598f9b2738b4eacc96398f17a8f
aliakbarzohour <aliakbarzohour@gmail.com>
2022-10-28 00:44:09 +0330

    clean code .

--- js/main.js
+++ js/main.js
@@ -5,15 +5,17 @@ const User_Url = "https://jsonplaceholder.typicode.com/users/";
 async function GetUsers(url, id) {
     let response = await fetch(url + id);
     let users = await response.json();
-
     return users;
 }
 
 async function GetArticles(url) {
     let response = await fetch(url);
     let data = await response.json();
+    // Loop in Json
     data.forEach(async(article) => {
+        // GetUser Function Call
         article.user = await GetUsers(User_Url, article.id)
+            // Append Data To App ...
         app.innerHTML += `
         <article>
             <h3 class="title">${article.title.toUpperCase()}</h3>
@@ -23,4 +25,6 @@ async function GetArticles(url) {
         `;
     });
 }
+
+// GetArticles Function Call
 GetArticles(Post_Url);
