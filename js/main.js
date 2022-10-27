6ea02dc744635c4c80d6c90cf4d4a64407d7e4a4
aliakbarzohour <aliakbarzohour@gmail.com>
2022-10-28 00:37:29 +0330

    Async To Async

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 const app = document.querySelector("#app");
-const Post_Url = "https://jsonplaceholder.typicode.com/posts";
+const Post_Url = "https://jsonplaceholder.typicode.com/posts/";
+const User_Url = "https://jsonplaceholder.typicode.com/users/";
+
+async function GetUsers(url, id) {
+    let response = await fetch(url + id);
+    let users = await response.json();
+
+    return users;
+}
 
 async function GetArticles(url) {
     let response = await fetch(url);
     let data = await response.json();
-    data.forEach((article) => {
+    data.forEach(async(article) => {
+        article.user = await GetUsers(User_Url, article.id)
         app.innerHTML += `
         <article>
             <h3 class="title">${article.title.toUpperCase()}</h3>
+            <h4 class="author">author: ${article.user.username.toUpperCase()}</h4>
             <p class="articleBody">${article.body}</p>
         </article>
         `;
